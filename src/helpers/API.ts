a34e9f5df1c8c5ed21225e1ae2e1c27663720f84
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-23 15:36:06 +0500

    add products request

---
+++ src/helpers/API.ts
@@ -0,0 +1,1 @@
+export const PREFIX = 'https://43cd34a29e27b783.mokky.dev';
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
+import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getMenu = async () => {
+    try {
+      const res = await fetch(`${PREFIX}/products`);
+      if (!res.ok) {
+        return;
+      }
+      const data = (await res.json()) as Product[];
+      setProducts(data);
+      console.log(products);
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <>
       <div className={styles['head']}>
@@ -11,14 +35,17 @@ export function Menu() {
         <Search placeholder='Введите блюдо или состав' />
       </div>
       <div>
-        <ProductCard
-          id={1}
-          name='Наслаждение'
-          description='Салями, руккола, помидоры, оливки'
-          rating={4.5}
-          price={300}
-          image='/product-demo.png'
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            description={product.description}
+            rating={product.rating}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
       </div>
     </>
   );
