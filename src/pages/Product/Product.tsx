67111a6f97c774d30f68f9979c1930852c46a155
Denis Kalkopf <deniskalkopf@gmail.com>
2024-05-16 14:01:03 +0500

    add product page style

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -6,12 +6,16 @@ import { PREFIX } from '../../helpers/API';
 import Headling from '../../components/Headling/Headling';
 import Button from '../../components/Button/Button';
 import styles from './Product.module.css';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { cartActions } from '../../store/cart.slice';
 
 export function Product() {
   const [product, setProduct] = useState<Product>();
   const [error, setError] = useState<string | undefined>();
-  const productId = useParams().id;
+  const productId = Number(useParams().id);
   const data = useLoaderData() as { data: Product };
+  const dispatch = useDispatch<AppDispatch>();
 
   const getProduct = async () => {
     try {
@@ -26,6 +30,11 @@ export function Product() {
     }
   };
 
+  const add = (e: MouseEvent) => {
+    e.preventDefault();
+    dispatch(cartActions.add(productId));
+  };
+
   return (
     <>
       <Suspense fallback={'Загрузка...'}>
@@ -37,9 +46,34 @@ export function Product() {
                   <img src='/arrow-back-icon.svg' width='25px' height='25px' alt='' />
                 </Link>
                 <Headling>{data.name}</Headling>
-                <Button className={styles['cart-button']}>
+                <Button className={styles['cart-button']} onClick={add}>
                   <img src='/cart-button-icon.svg' alt='иконка корзины' />В корзину
                 </Button>
+              </div>
+              <div className={styles['content']}>
+                <img src={data.image} className={styles['image']} alt={data.name} />
+                <div className={styles['text']}>
+                  <div className={styles['price']}>
+                    Цена{' '}
+                    <span className={styles['currency']}>
+                      {data.price} <span>₽</span>
+                    </span>
+                  </div>
+                  <div className={styles['rating']}>
+                    Рейтинг{' '}
+                    <div className={styles['rate']}>
+                      {data.rating} <img src='/star-icon.svg' alt='иконка звезды' />
+                    </div>
+                  </div>
+                  <div className={styles['ingredients']}>
+                    Состав:
+                    <ul>
+                      {data.ingredients.map((ingredient) => (
+                        <li>{ingredient}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
               </div>
             </>
           )}
