f0282008121cd99ef1d1381ac800c7ae6e835270
Denis Kalkopf <deniskalkopf@gmail.com>
2024-05-19 22:07:23 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: 'https://denis-pizza-app.netlify.app',
+  base: 'https://denis-pizza-app.netlify.app',
   plugins: [react()],
 });
