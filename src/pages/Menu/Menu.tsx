b4d2e3d4bc013469ae01616e299455079123fc9d
Denis Kalkopf <deniskalkopf@gmail.com>
2024-06-07 11:56:19 +0500

    add search

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,41 +1,52 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import Headling from '../../components/Headling/Headling';
-//import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 import axios, { AxiosError } from 'axios';
 import { MenuList } from './MenuList/MenuList';
+import Search from '../../components/Search/Search';
 
 export default function Menu() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | undefined>();
+  const [filter, setFilter] = useState<string>();
 
   useEffect(() => {
-    const getMenu = async () => {
-      try {
-        setIsLoading(true);
+    getMenu(filter);
+  }, [filter]);
+
+  const getMenu = async (query?: string) => {
+    try {
+      setIsLoading(true);
+      if (query) {
+        const { data } = await axios.get<Product[]>(`${PREFIX}/products?name=${query}*`);
+        setProducts(data);
+      } else {
         const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
         setProducts(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-        if (error instanceof AxiosError) {
-          setError(error.message);
-        }
-        setIsLoading(false);
-        return;
       }
-    };
-    getMenu();
-  }, []);
+      setIsLoading(false);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof AxiosError) {
+        setError(e.message);
+      }
+      setIsLoading(false);
+      return;
+    }
+  };
+
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
 
   return (
     <>
       <div className={styles['head']}>
         <Headling>Menu</Headling>
-        {/* <Search placeholder='Введите блюдо или состав' /> */}
+        <Search placeholder='Введите название...' onChange={updateFilter} />
       </div>
       <div>
         {error && <>{error}</>}
